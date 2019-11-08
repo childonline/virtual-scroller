e916fcf359bbd4361fc87d94d25705bd95cb2eb1
Eugen Schneider <schneider.eugen@hotmail.com>
2019-11-08 17:06:47 +0200

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-right color="blue-grey" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-spacer/>
+    </v-app-bar>
+
+    <v-content>
+      <RecycleScroller v-if="list" v-slot="{ item }" :items="list" key-field="id" :item-size="128" direction="horizontal" style="height: 100%">
+        <v-sheet tile style="width: 128px">
+          <span class="d-flex body-1 cell">{{ item.id }}</span>
+          <span class="d-flex body-1 cell">{{ $moment(item.date, "YYYY-MM-DDTHH:mm:ss.SSS").format("YYYY-MM-DD") }}</span>
+
+          <input v-model="item.available" class="cell"/>
+
+          <v-row no-gutters>
+            <v-col cols="4">
+              <v-btn v-model="item.booked" color="orange" x-small icon/>
+            </v-col>
+            <v-col cols="4">
+              <v-btn v-model="item.close_out" color="orange" x-small icon/>
+            </v-col>
+            <v-col cols="4">
+              <v-btn v-model="item.close_out" color="red" x-small icon/>
+            </v-col>
+          </v-row>
+
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+          <input v-model="item.available" class="cell"/>
+
+        </v-sheet>
+      </RecycleScroller>
+    </v-content>
+
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+<script>
+
+  export default {
+
+    async created() {
+      let list1 = await this.$axios
+        .get('http://5dc52f170bbd050014fb8659.mockapi.io/inventory?p=1&l=100')
+        .then(response => {
+          return response.data
+        });
+
+      let list2 = await this.$axios
+        .get('http://5dc52f170bbd050014fb8659.mockapi.io/inventory?p=1&l=100')
+        .then(response => {
+          return response.data
+        });
+
+      let list3 = await this.$axios
+        .get('http://5dc52f170bbd050014fb8659.mockapi.io/inventory?p=1&l=100')
+        .then(response => {
+          return response.data
+        });
+
+      this.list = [...list1, ...list2, ...list3]
+    },
+
+    data: () => ({
+      list: [],
+      drawer: null
+    }),
+  };
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .cell {
+    width: 128px;
+    text-align: center;
+    outline-color: transparent;
+  }
+
+  .hover .cell {
+
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import axios from 'axios';
+import moment from 'moment';
+
+Vue.prototype.$axios = axios;
+Vue.prototype.$moment = moment;
+
+import VueVirtualScroller from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+Vue.use(VueVirtualScroller);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
